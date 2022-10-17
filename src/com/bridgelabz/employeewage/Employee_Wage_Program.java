8395e7347aeddad7d4e134b6b0dbfc0966cadb23
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-17 20:53:26 +0530

    Adding Daily Wage Checker

--- src/com/bridgelabz/employeewage/Employee_Wage_Program.java
+++ src/com/bridgelabz/employeewage/Employee_Wage_Program.java
@@ -3,15 +3,21 @@ package com.bridgelabz.employeewage;
 import java.util.Random;
 
 public class Employee_Wage_Program {
+	final static int wagePerHour = 20;
+	static int empHrs;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Program");
-		Random random=new Random();
-		int isPresent=random.nextInt(2);
-		System.out.println(isPresent);
-		if(isPresent==0) {
+		Random random = new Random();
+		int isPresent = random.nextInt(2);
+		if (isPresent == 0) {
 			System.out.println("Employee is present");
-		}else {
+			empHrs = 8;
+		} else {
 			System.out.println("Employee is absent");
+			empHrs = 0;
 		}
+		int dailyWage = wagePerHour * empHrs;
+		System.out.println("Daily Employee wage: " + dailyWage);
 	}
 }
