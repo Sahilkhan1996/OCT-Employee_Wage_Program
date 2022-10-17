baf385eb4db2091a4604f5962fa50441cb37bd20
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-17 21:10:25 +0530

    Adding Part time Employee wage program

--- src/com/bridgelabz/employeewage/Employee_Wage_Program.java
+++ src/com/bridgelabz/employeewage/Employee_Wage_Program.java
@@ -8,11 +8,15 @@ public class Employee_Wage_Program {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Program");
-		Random random = new Random();
-		int isPresent = random.nextInt(2);
+		Random random = new Random(); // generating random number
+		int isPresent = random.nextInt(3);
+		// Checking if the employee is present full time/part time/absent
 		if (isPresent == 0) {
-			System.out.println("Employee is present");
+			System.out.println("Employee is present Full time");
 			empHrs = 8;
+		} else if (isPresent == 1) {
+			System.out.println("Employee is present Part time");
+			empHrs = 4;
 		} else {
 			System.out.println("Employee is absent");
 			empHrs = 0;
