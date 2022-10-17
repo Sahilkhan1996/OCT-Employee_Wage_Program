a09117a525eff3272273d8d2f1eeb9047e4755fe
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-17 20:35:44 +0530

    Updating to check if the employee is present

--- src/com/bridgelabz/employeewage/Employee_Wage_Program.java
+++ src/com/bridgelabz/employeewage/Employee_Wage_Program.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.employeewage;
 
+import java.util.Random;
+
 public class Employee_Wage_Program {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Program");
+		Random random=new Random();
+		int isPresent=random.nextInt(2);
+		System.out.println(isPresent);
+		if(isPresent==0) {
+			System.out.println("Employee is present");
+		}else {
+			System.out.println("Employee is absent");
+		}
 	}
 }
