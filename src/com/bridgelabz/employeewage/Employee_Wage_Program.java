73a2bfe98394e71baa180aa7d5adebe6d10b8469
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-17 21:19:47 +0530

    Refactoring the code with the switch case

--- src/com/bridgelabz/employeewage/Employee_Wage_Program.java
+++ src/com/bridgelabz/employeewage/Employee_Wage_Program.java
@@ -3,7 +3,11 @@ package com.bridgelabz.employeewage;
 import java.util.Random;
 
 public class Employee_Wage_Program {
+
+	static final int IS_Full_Time = 0;
+	static final int IS_PART_TIME = 1;
 	final static int wagePerHour = 20;
+
 	static int empHrs;
 
 	public static void main(String[] args) {
@@ -11,16 +15,22 @@ public class Employee_Wage_Program {
 		Random random = new Random(); // generating random number
 		int isPresent = random.nextInt(3);
 		// Checking if the employee is present full time/part time/absent
-		if (isPresent == 0) {
+		// with the switch case
+		switch (isPresent) {
+		case IS_Full_Time: {
 			System.out.println("Employee is present Full time");
 			empHrs = 8;
-		} else if (isPresent == 1) {
+		}
+			break;
+		case IS_PART_TIME: {
 			System.out.println("Employee is present Part time");
 			empHrs = 4;
-		} else {
+		}
+		default:
 			System.out.println("Employee is absent");
 			empHrs = 0;
 		}
+
 		int dailyWage = wagePerHour * empHrs;
 		System.out.println("Daily Employee wage: " + dailyWage);
 	}
