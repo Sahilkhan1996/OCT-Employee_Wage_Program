6533f65ba366a499f0c05c8ff4360414dbb3c215
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-17 20:30:13 +0530

    Initiating the employee wage program

---
+++ src/com/bridgelabz/employeewage/Employee_Wage_Program.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeewage;
+
+public class Employee_Wage_Program {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Program");
+	}
+}
