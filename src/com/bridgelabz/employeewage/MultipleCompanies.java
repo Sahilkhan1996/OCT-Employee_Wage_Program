872f5b7809c5acb808d3b8507ed96b3a5e6ab251
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-06 16:52:13 +0530

    Updating progrma with to string to get the total wage of the company

--- src/com/bridgelabz/employeewage/MultipleCompanies.java
+++ src/com/bridgelabz/employeewage/MultipleCompanies.java
@@ -4,9 +4,11 @@ public class MultipleCompanies {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Program");
 		// company name, wAGE_PER_HOUR, mAX_WORKING_DAYS, mAX_WORKING_HOURS
-		Employee_Wage_Program emp1 = new Employee_Wage_Program("BridgeLabz", 20, 20, 80);
-		emp1.totalwage();
-		Employee_Wage_Program emp2 = new Employee_Wage_Program("TCS", 10, 10, 60);
-		emp2.totalwage();		
+		Employee_Wage_Program bridgeLabz = new Employee_Wage_Program("BridgeLabz", 20, 20, 80);
+		bridgeLabz.totalwage();
+		System.out.println(bridgeLabz); //from to string 
+		Employee_Wage_Program tCS = new Employee_Wage_Program("TCS", 10, 10, 60);
+		tCS.totalwage();		
+		System.out.println(tCS);//from to string
 	}
 }
